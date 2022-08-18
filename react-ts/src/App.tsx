03a5f510ac7a5e01e0deac629590e88e6ccff3b6
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-18 09:18:37 +0900

    型定義なしでデータを扱う

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Todo } from './Todo';
 
 function App() {
+  const [todoList, setTodoList] = useState<any>([]);
   const onClickFetchData = () => {
     axios.get('https://jsonplaceholder.typicode.com/todos').then((res) => {
-      console.log(res);
+      setTodoList(res.data);
     });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todoList.map((todo) => (
+        <Todo title={todo.title} userId={todo.userId} />
+      ))}
     </div>
   );
 }
